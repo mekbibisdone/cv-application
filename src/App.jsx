7430131721263300fcf7b74b707b27497adedb84
mekbibDev <mekbibtare@gmail.com>
2023-12-24 11:21:03 +0300

    Create the html elements for displaying the cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import Form from "./Form"
-function App() {
+import Form from "./Form";
+import CV from "./CV";
 
+function App() {
   return (
     <>
-    <header><h1>CV Application</h1></header>
-    <main>
-    <Form></Form>
-    </main>
-    
+      <main>
+        <section>
+          <h1>Form</h1>
+          <Form></Form>
+        </section>
+        <section>
+          <h1>CV</h1>
+          <CV></CV>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/CV.jsx
@@ -0,0 +1,31 @@
+export default function CV (){
+  return (
+    <>
+      <section>
+        <h1>Contact</h1>
+        <h2>FirstName LastName</h2>
+        <a href="mailto:">email@domain.com</a>
+        <a href="tel:">+xxxxxxxx</a>
+      </section>
+      <section>
+        <h1>Education</h1>
+        <h2>School Name: </h2>
+        <h2>Title of study:</h2>
+        <h3>From:</h3>
+        <time>year:month:day</time>
+        <h3>To:</h3>
+        <time>year:month:day</time>
+      </section>
+      <section>
+        <h1>Practical Experience</h1>
+        <h2>Company Name:</h2>
+        <h2>Responsibilities:</h2>
+        <ul></ul>
+        <h3>From:</h3>
+        <time>year:month:day</time>
+        <h3>To:</h3>
+        <time>year:month:day</time>
+      </section>
+    </>
+  )
+}
