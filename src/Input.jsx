4b29c6064c0ff8d6b11d07cc9af8092c7b2520b6
mekbibDev <mekbibtare@gmail.com>
2023-12-24 15:40:41 +0300

    Handle input with the form component

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,8 +1,24 @@
-export default function Input({ label, labelText, type, min = null, max = null }) {
+export default function Input({
+  label,
+  labelText,
+  type,
+  min = null,
+  max = null,
+  value,
+  handleChange,
+}) {
   return (
     <>
       <label htmlFor={label}>{labelText}</label>
-      <input type={type} name={label} id={label} min={min} max={max} />
+      <input
+        type={type}
+        name={label}
+        id={label}
+        min={min}
+        max={max}
+        value={value}
+        onChange={handleChange}
+      />
     </>
   );
 }
